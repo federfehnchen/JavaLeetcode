df3b6026e6616e6fa97eba256eb113d3544f84eb
federfehnchen <felixfarke@icloud.com>
2022-03-10 23:50:18 +0100

    firstpos

--- src/main.java
+++ src/main.java
@@ -4,12 +4,8 @@ public class main {
 
     public static void main(String[] args) {
 
-        int[] nums = {1,3,2};
-        nextPermutation(nums);
-        for(int i:nums)
-        {
-            System.out.println(i);
-        }
+        int[] nums = {0,1,2};
+        System.out.println(firstMissingPositive(nums));
 
     }
 
@@ -603,4 +599,28 @@ public class main {
             }
             return;
         }
+
+    public static int firstMissingPositive(int[] nums)
+    {
+        int i = 0;
+        while( i < nums.length){
+            if(nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]){
+                swap(nums,i,nums[i] - 1);
+            }else{
+                i++;
+            }
+        }
+        for(i = 0;i<nums.length;i++){
+            if(nums[i] != i + 1){
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    private static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
